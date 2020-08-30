df2c163b9b37a0b8c88fa47c8dbd26590e8582da
Herbert Soares <o_herbert@hotmail.com>
2020-08-30 19:33:20 -0300

    Acertando o grafico part 3

--- Entities/Grafico.cs
+++ Entities/Grafico.cs
@@ -11,10 +11,14 @@ namespace Arduino_teste2.Entities
 {
     class Grafico : Form
     {
-        // Exlcusiva para a classe Registro
-        private ChartValues<Registro>[] ChartValues1 = new ChartValues<Registro>[2]; 
+        // Exlcusiva para a classe Registro editar o numero de pontos
 
-        // 
+        private static String[] pontosGrafico = { "Zona 1", "Zona 2", "Zona 3", "Zona 4" };
+
+        private ChartValues<Registro>[] chartValues = new ChartValues<Registro>[pontosGrafico.Length];
+        
+
+        // Geral
         public LiveCharts.Wpf.CartesianChart CartesianChart { get; set; }
         public int Intervalo { get; set; } //Em segundos
         private Timer Timer = new Timer();
@@ -42,9 +46,10 @@ namespace Arduino_teste2.Entities
         {
             
             List<LineSeries> lines = new List<LineSeries>();
-            addLine(lines,"Zona1");
-            addLine(lines,"Zona2");
 
+            foreach (String ponto in pontosGrafico) {
+                addLine(lines, ponto);
+            }
 
             foreach (LineSeries line in lines)
             {
@@ -82,28 +87,26 @@ namespace Arduino_teste2.Entities
         private void TimerOnTick(object sender, EventArgs eventArgs)
         {
             var now = DateTime.Now;
-            /*
-            foreach (ChartValues<Registro> chart in ChartValues1) { 
-                chart.
-            }*/
-
-            ChartValues1[0].Add(new Registro
-            {
-                DateTime = now,
-                Valor = R.Next(0, 157)
-            });
-
-            ChartValues1[1].Add(new Registro
-            {
-                DateTime = now,
-                Valor = R.Next(160, 167)
-            });
+            
+            foreach (ChartValues<Registro> chart in chartValues) { 
+                chart.Add(new Registro
+                {
+                    DateTime = now,
+                    Valor = R.Next(0, 157)
+                });
+            }
 
             SetAxisLimits(now);
 
             //lets only use the last 30 values
-            if (ChartValues1[0].Count > 30) ChartValues1[0].RemoveAt(0);
-            if (ChartValues1[1].Count > 30) ChartValues1[1].RemoveAt(0);
+
+            foreach (ChartValues<Registro> chart in chartValues)
+            {
+                if (chart.Count > 30) chart.RemoveAt(0);
+            }
+
+
+            
         }
 
         private void addLine (List<LineSeries> lines, string titulo) {
@@ -114,37 +117,18 @@ namespace Arduino_teste2.Entities
                 .Y(model => model.Valor);        //use the value property as Y
 
             Charting.For<Registro>(mapper1);
-            ChartValues1[lines.Count] = new ChartValues<Registro>();
+            chartValues[lines.Count] = new ChartValues<Registro>();
 
 
             LineSeries line = new LineSeries
             {
                 Title = titulo,
-                Values = ChartValues1[lines.Count],
+                Values = chartValues[lines.Count],
                 PointGeometrySize = 18,
                 StrokeThickness = 4
             };
 
-
-            /*
-            var mapper2 = Mappers.Xy<Registro>()
-                .X(model => model.DateTime.Ticks)   //use DateTime.Ticks as X
-                .Y(model => model.Valor);          //use the value property as Y
-
-            Charting.For<Registro>(mapper2);
-            ChartValues2 = new ChartValues<Registro>();
-
-            LineSeries line2 = new LineSeries()
-            {
-                Title = "Zona 2",
-                Values = ChartValues2,
-                PointGeometrySize = 18,
-                StrokeThickness = 4
-            };
-            */
-
-            lines.Add(line);
-           
+            lines.Add(line);         
             
         }
 
